86887d7a0663120ce97a25b04155abf89e04f680
satya <satya143@gmail.com>
2023-03-14 06:32:51 +0530

    Added:Welcome to hotel reservation system

---
+++ src/com/hotelReservationSystem/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.hotelReservationSystem;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        System.out.println("welcome to hotel reservation System");
+    }
+}
